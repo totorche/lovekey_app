6c50b9f527dc8e01c4f7ec4e0572ab111b091c0c
Hervé Torche <herve.torche@fbw-one.com>
2013-10-22 18:53:47 +0200

    Test son touches

--- js/configurateur.js
+++ js/configurateur.js
@@ -69,6 +69,7 @@ function init_app(){
   $(window).resize(window_change_size);
 
   // charge le son d'un click
+  alert(getPhoneGapPath() + 'mouseclick.wav');
   clickSound = new Media(getPhoneGapPath() + 'mouseclick.wav');
   // clickSound = new Media('http://lovekey.com/mouseclick.wav');
   // clickSound = new Media('mouseclick.wav');
